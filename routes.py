6353d40475aa491daf382791085f5ab0dff7e556
Robin Kasparek <r.kasparek@web.de>
2026-05-12 21:12:01 +0200

    Revert "Add barebones plugin with basic routing and UI scaffold"
    This reverts commit c9de9641accf74c5326dbe1fed51178184a25d0f.

--- routes.py
+++
@@ -1,6 +0,0 @@
-from fastapi import FastAPI
-
-def setup(app: FastAPI, context: dict):
-    @app.get("/api/plugins/subway_scaler/status")
-    def get_status():
-        return {"status": "ok", "message": "Subway Scaler is ready"}
